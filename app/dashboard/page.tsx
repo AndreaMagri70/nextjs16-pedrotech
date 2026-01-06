2fbb2cfcff0cb11f1a6870a81f23c77df13d0c1d
AndreaMagri70 <assomag@libero.it>
2026-01-06 18:27:09 +0100

    Fix: prima parte pagina Dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,32 @@
 import Sidebar from "@/components/sidebar";
+import getCurrentUser from "@/lib/auth";
+import { prisma } from "@/lib/prisma";
 
-export default function DashboardPage() {
+export default async function DashboardPage() {
+  const user = await getCurrentUser();
+  const userId = user.id;
+  
+  const totalProducts = await prisma.product.count({ where: { userId } });
+  const lowStock = await prisma.product.count({
+    where: {
+      userId,
+    },
+  });
+  const recent = await prisma.product.findMany({
+    where: { userId },
+    orderBy: { createdAt: "desc"},
+    take: 5,
+  });
+  const allProducts = await prisma.product.findMany({
+    where: { userId },
+    select: { price: true, quantity: true, createdAt: true },
+  });
+
+  const totalValue = allProducts.reduce(
+    (sum, product) => sum + Number(product.price) * Number(product.quantity), 
+  0
+);
+
   return (
     <div className="min-h-screen bg-gray-50">
       <Sidebar currentPath="/dashboard" />
@@ -11,6 +37,19 @@ export default function DashboardPage() {
             <div>
               <h1 className="text-2xl font-semibold text-gray-900">Dashboard</h1>
               <p className="text-sm text-gray-500">Welcome back! Here is an overview of your Inventory</p>
+            </div>
+          </div>
+        </div>
+        {/* Key Metrics */}
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 mb-8">
+          <div className="bg-white rounded-lg border border-gray-200 p-6">
+            <h2 className="text-lg font-semibold text-gray-900 mb-6">
+              Key Metrics 
+            </h2>
+            <div className="grid grid-cols-3 gap-6">
+              <div className="text-center">
+                <div>{totalProducts}</div>
+              </div>
             </div>
           </div>
         </div>
