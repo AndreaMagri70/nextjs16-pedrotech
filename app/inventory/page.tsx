9cb11c4c04236e83efe5d63f0a385a7cc0c58182
AndreaMagri70 <assomag@libero.it>
2026-01-06 17:17:14 +0100

    Fix:prima parte sidebar

---
+++ app/inventory/page.tsx
@@ -0,0 +1,13 @@
+import Sidebar from "@/components/sidebar";
+
+export default function InventoryPage() {
+  return (
+      <div className="min-h-screen bg-gray-50">
+              <Sidebar currentPath="/inventory" />
+     
+      </div>
+    
+
+  
+  )
+}
--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 
 export default function SignInPage() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-purple-50 to-purple-100">
-        <div className="max-w-md w-full space-y-8">
+    <div className="min-h-screen flex items-center justify-center bg-linear-to-br from-purple-50 to-purple-100 m-auto">
+        <div className="w-100 p-2 m-auto bg-gray-500">
             <SignIn />
             <Link href="/" className="block text-center mt-4 text-violet-400 hover:underline"> Go back HOME </Link>
         </div>
