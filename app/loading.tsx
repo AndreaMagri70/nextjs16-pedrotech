eaa8577935a8b6c817eacaeba4654520bc9fb528
AndreaMagri70 <assomag@libero.it>
2026-01-11 18:30:37 +0100

    Fix: realizzata la Settings Page

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,6 +1,13 @@
 export default function Loading() {
-
-  // Stack uses React Suspense, which will render this page while user data is being fetched.
-  // See: https://nextjs.org/docs/app/api-reference/file-conventions/loading
-  return <></>;
+  return (
+    <div className="flex flex-col items-center justify-center min-h-100 w-full space-y-4">
+      {/* Spinner animato viola */}
+      <div className="w-12 h-12 border-4 border-purple-200 border-t-purple-600 rounded-full animate-spin"></div>
+      
+      {/* Testo di cortesia */}
+      <p className="text-gray-500 font-medium animate-pulse">
+        Caricamento inventario...
+      </p>
+    </div>
+  );
 }
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -15,46 +15,49 @@ export default function Sidebar({
         { name: "Settings", href: "/settings", icon: Settings },
     ];
 
-  return (
-    <div className="fixed left-0 top-0 bg-gray-900 text-white w-64 min-h-screen p-6 z-10">
-        <div className="mb-8">
-            <div className="flex items-centersoace-x-2 mb-4">
-                <Image 
-                src="/JellowSunDev-web.png"
-                alt="JellowSunDev"
-                width={75}
-                height={75}
-                className="rounded-lg"
-                />
-            </div>
-            <span className="text-lg font-semibold">Inventory App</span>
-        </div>
-        <nav className="space-y-1">
-            <div className="text-sm font-bold text-gray-400 uppercase">
-                Inventory
-            </div>
-            {navigation.map((item, key) => {
-                const IconComponent = item.icon;
-                const isActive = currentPath === item.href;
-                return (<Link 
-                        href={item.href} 
-                        key={key}
-                        className={`flex items-center space-x-3 py-2 px-3 rounded-lg ${
-                            isActive 
-                            ? " bg-purple-100 text-gray-800" 
-                            : " hover:bg-gray-800 text-gray-300"
-                        }`}>
-                    <IconComponent className="w-5 h-5"/>
-                    <span className="text-sm">{item.name}</span>
+    return (
+        <div className="fixed left-0 top-0 bg-gray-900 text-white w-64 min-h-screen p-6 z-10">
+            <div className="mb-8">
+                <div className="flex items-centersoace-x-2 mb-4">
+                    <Link href="/dashboard">
+                        <Image
+                            src="/JellowSunDev-web.png"
+                            alt="JellowSunDev"
+                            width={75}
+                            height={75}
+                            className="rounded-lg"
+                        />
+                    </Link>
+                </div>
+                <Link href="/dashboard">
+                <span className="text-lg font-semibold">Inventory App</span>
                 </Link>
-                );
-            })}
-        </nav>
-        <div className="absolute bottom-0 left-0 right-0 border-t border-gray-700">
-            <div className="flex items-center justify-between">
-                <UserButton showUserInfo/>
+            </div>
+            <nav className="space-y-1">
+                <div className="text-sm font-bold text-gray-400 uppercase">
+                    Inventory
+                </div>
+                {navigation.map((item, key) => {
+                    const IconComponent = item.icon;
+                    const isActive = currentPath === item.href;
+                    return (<Link
+                        href={item.href}
+                        key={key}
+                        className={`flex items-center space-x-3 py-2 px-3 rounded-lg ${isActive
+                                ? " bg-purple-100 text-gray-800"
+                                : " hover:bg-gray-800 text-gray-300"
+                            }`}>
+                        <IconComponent className="w-5 h-5" />
+                        <span className="text-sm">{item.name}</span>
+                    </Link>
+                    );
+                })}
+            </nav>
+            <div className="absolute bottom-0 left-0 right-0 border-t border-gray-700">
+                <div className="flex items-center justify-between">
+                    <UserButton showUserInfo />
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
