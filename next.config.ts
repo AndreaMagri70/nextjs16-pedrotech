f1b14a66c8bf21111c857da57c96cb4c5982f557
AndreaMagri70 <assomag@libero.it>
2026-01-13 10:01:15 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-   DATABASE_URL: process.env.DATABASE_URL,
+  /* config options here */
+  env: {
+    DATABASE_URL: process.env.DATABASE_URL,
+  },
 };
 
 export default nextConfig;
