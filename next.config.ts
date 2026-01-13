e974c8ccf6c1833c9fcb52074f14c3651d1e90c3
AndreaMagri70 <assomag@libero.it>
2026-01-13 09:58:00 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+   DATABASE_URL: process.env.DATABASE_URL,
 };
 
 export default nextConfig;
